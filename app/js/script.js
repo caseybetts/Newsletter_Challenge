f7f780c74a04ac3af73ebc804f5f547317fc8bb6
caseybetts <casey.betts@colorado.edu>
2022-12-05 09:15:23 -0700

    End of week changes

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,29 @@
-console.log('hello world');
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(header.classList.contains('open')){ // Close hamburger menu
+        body.classList.remove('noscroll')
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        })
+
+    }
+    else { // Open hamburger menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+
+    }
+    
+});
